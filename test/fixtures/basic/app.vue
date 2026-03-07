df248a3c3a36fc0fb3db4d41b529dfeebe18123f
Aleks <aleksandar.trpkovski@gmail.com>
2026-03-07 17:12:27 +1100

    added test

---
+++ test/fixtures/basic/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Basic Fixture</h1>
+  </div>
+</template>
---
+++ test/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,11 @@
+import McpBModule from '../../../src/module'
+
+export default defineNuxtConfig({
+  modules: [McpBModule],
+
+  mcpB: {
+    autoInitialize: true,
+  },
+
+  compatibilityDate: '2025-01-01',
+})
